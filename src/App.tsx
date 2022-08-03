1505ac7d3612ff0769255e2d19aadd328f105703
SauloOliveira06 <oliveira.saulo06@gmail.com>
2022-08-03 11:35:55 -0400

    clean architecture app

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import text from './styles/text.module.css';
+
+const App = () => {
+  return (
+    <div className={text.textCenter}>
+      Hello, there!!
+    </div>
+  )
+}
+
+export default App
